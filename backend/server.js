071720a3a9b1877b9a493cb7ded2c81a97c4d812
FBHorizon786 <fbhorizon786@gmail.com>
2026-04-30 00:11:37 +0500

    fix cors final

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ import nodemailer from "nodemailer";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+
 
 const frontends = [
   process.env.FRONTEND_URL || "http://localhost:3000",
@@ -29,7 +31,7 @@ app.use(cors({
     "https://fintech-v2-uz2y5jn3o-fbhorizons-projects.vercel.app",
 
     "https://fintechbalance.net",
-    "https://www.fintechbalance.net" // ⭐ MOST IMPORTANT
+    "https://www.fintechbalance.net" // ⭐ MUST
   ],
   methods: ["GET", "POST"],
 }));
