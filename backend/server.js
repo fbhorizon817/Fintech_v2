ea42ee930ef6a66a19b771190bed1775fcd84110
FBHorizon786 <fbhorizon786@gmail.com>
2026-04-29 12:21:08 +0500

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,9 @@ const frontends = [
 //   methods: ["GET", "POST"],
 //   allowedHeaders: ["Content-Type"],
 // }));
+
+app.use(cors());
+
 app.use(cors({ origin: ['http://localhost:3000','http://fintechbalance.net','https://fintech-v2.vercel.app'] }));
 app.use(express.json({ limit: "10kb" }));
 
